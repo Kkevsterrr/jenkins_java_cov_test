2ccb0a7553806ae41b257933839c446e3c0f43a5
gaozebao <gaozebao@gmail.com>
2017-09-11 20:03:27 -0400

    comment out test for MaxSubArray

--- src/test/java/MaxSubArray/MaxSubArrayTest.java
+++ src/test/java/MaxSubArray/MaxSubArrayTest.java
@@ -7,24 +7,24 @@ import org.junit.Test;
 
 public class MaxSubArrayTest {
 
-	private MaxSubArray msa;
-	
-	@Before
-	public void setUp() {
-		msa = new MaxSubArray();
-	}
-	
-	@Test
-	public void testMaxSubSum() {
-		int[] a = new int[] {1, -2, 3, 10, -4, 7, 2, -5};
-		assertEquals(18, msa.getMaxSum(a));
-		int[] b = new int[] {-2, 1, 3, -50, 60};
-		assertEquals(60, msa.getMaxSum(b));
-	}
-	
-	@Test
-	public void testMaxSingleNeg() {
-		int[] a = new int[] {-1, -2, -3, -10, -4, -7, -2, -5};
-		assertEquals(-1, msa.getMaxSum(a));
-	}
+//	private MaxSubArray msa;
+//	
+//	@Before
+//	public void setUp() {
+//		msa = new MaxSubArray();
+//	}
+//	
+//	@Test
+//	public void testMaxSubSum() {
+//		int[] a = new int[] {1, -2, 3, 10, -4, 7, 2, -5};
+//		assertEquals(18, msa.getMaxSum(a));
+//		int[] b = new int[] {-2, 1, 3, -50, 60};
+//		assertEquals(60, msa.getMaxSum(b));
+//	}
+//	
+//	@Test
+//	public void testMaxSingleNeg() {
+//		int[] a = new int[] {-1, -2, -3, -10, -4, -7, -2, -5};
+//		assertEquals(-1, msa.getMaxSum(a));
+//	}
 }
